45fff4cef71e40f7a8a4355fd6c27f44ddb46b83
bestsilver <dkvkxm99@gmail.com>
2022-07-09 21:35:30 +0900

    feat: 로그 설정하기

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -6,6 +6,8 @@ import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
     private MemberRepository memberRepository;
     private JwtTokenProvider jwtTokenProvider;
+
+    private static final Logger logger = LoggerFactory.getLogger("file");
+
 
     public AuthService(MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider) {
         this.memberRepository = memberRepository;
@@ -25,6 +30,7 @@ public class AuthService {
         member.checkPassword(request.getPassword());
 
         String token = jwtTokenProvider.createToken(request.getEmail());
+        logger.info("login successfully : " + member.getEmail());
         return new TokenResponse(token);
     }
 
--- src/main/java/nextstep/subway/map/application/PathService.java
+++ src/main/java/nextstep/subway/map/application/PathService.java
@@ -7,13 +7,17 @@ import nextstep.subway.map.domain.SubwayPath;
 import nextstep.subway.station.domain.Station;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class PathService {
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
     public SubwayPath findPath(List<Line> lines, Station source, Station target) {
         SubwayGraph graph = new SubwayGraph(SectionEdge.class);
         graph.addVertexWith(lines);
@@ -23,6 +27,9 @@ public class PathService {
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
         GraphPath<Station, SectionEdge> path = dijkstraShortestPath.getPath(source, target);
 
+        jsonLogger.info("{}, {}",
+                kv("source", source.getName()),
+                kv("target", target.getName()));
         return convertSubwayPath(path);
     }
 
